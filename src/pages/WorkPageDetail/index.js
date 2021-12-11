43eafeb598619b5ba6995ffb470803d65f44e3cb
Yuehao Shi <ashimoon97@gmail.com>
2021-12-11 07:45:08 -0600

    Update121121

--- src/pages/WorkPageDetail/index.js
+++ src/pages/WorkPageDetail/index.js
@@ -24,13 +24,13 @@ export default () =>{
                 <p>
                 To simulate the solar-earth system, two functions, “spheres” and “orbits” were defined to represent the shape and movement trace of sun and earth with “graph_objects” module in Python Plotly. To better illustrate the visualization effect, the diameter of the earth sphere was made ten times larger than the real size. The system then was constructed with “Layout” method in graph_objects module by applying the sizes of sun and earth in “spheres” function and the distance between sun and earth into the “orbits” function. The generated system is shown as Figure 1 below:
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis1.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis1.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 1: Model of earth-solar system</center></p>
                 <p>
                 The goal of this part of the project was to visually illustrate why solar energy varies in the different time and location of the earth surface. To achieve this goal, the color of the earth was modified as gradient color, with brighter color on the side of the earth towards sun, and darker color on the side of the earth backwards to the sun. The surface color of the earth was set to be positively related to the coordinate of the point on the sphere. More specifically, the color of each pixel on the surface of the sphere was set to be proportional to “x0**2 - y0**2 - z0**2”, where x0, y0, and z0 were the coordinates to each point.
 After changing the surface color, the solar-earth system was shown in the Figure 2 below, where the surface of the earth was brighter in the side closer to the sun and vice versa. This model shows the basic principle of the solar energy application in different places of the earth: generally speaking, the equator area where can get most of the direct sunlight, has more sunlight intensity compared to polar areas of the earth, thus has more efficiency if applied infrastructures by solar energy industry.
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis2.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis2.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 2: Visualization about sunlight effect to earth</center></p>
                 <p>
                 <b>Part 2: Peak Sun Hour (PSH) Distribution in Different US States</b>
@@ -41,17 +41,17 @@ After changing the surface color, the solar-earth system was shown in the Figure
                 <p>
                 The first step of this part was to import the data of state boundaries in the US. The geography information was retrieved through the Boundary Shapefile from United States Census Bureau in 2018. The Boundary Shapefile file was imported through “mpl_toolkits.basecamp” module in Python, which shows the boundary of states, as shown in Figure 3 below.
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis3.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis3.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 3: States boundary of US</center></p>
                 <p>
                 The next step was to implement the information of PSH into different colors on the boundary map. This step was achieved by the “Polygon” method in “matplotlib” method in Python. The PSH data were acquired from Renogy (Renogy, 2013). To match the PSH value with corresponding state by state name, the values of PSHs were shown in the “YlOrRd” colormap, with higher PSH having darker red color. Meanwhile, the state of Hawaii and Alaska were also move to the left bottom corner of the map to make the map in a proper scale. By setting the original boundary to be transparent and adding the corresponding color legend bar, the generated map was shown as Figure 4 below.
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis4.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis4.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 4: PSH map for each state</center></p>
                 <p>
                 To better illustrate the solar energy usage potential of the states, the map was divided into five different solar energy application level. This step was inspired by the application of drawing contours in UIUC CS519 class. The generated map in the last step was saved and each pixel of it was read and stored in a new list containing the RGB values. The RGB values were then converted into each unique value by “65536 * R + 256 * G + B”, which was later used as the parameter to draw a contour based on different color level with “contour” method in “matplotlib” module in Python. The contour was divided in five different level. The colormap used for this figure was “summer”, where the darker green region in the map indicates better potential and benefits for developing solar panel as a kind of “green energy” implementation.
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis5.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis5.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 5: Solar Energy Usage Potential Regions</center></p>
                 <p>
                 <b>Part 3: Best Solar Panel Usage Angle</b>
@@ -89,12 +89,12 @@ After changing the surface color, the solar-earth system was shown in the Figure
                 <p>
                 The first step of this part was to calculate the solar panel light intensity using the equation mentioned above in tilts between 0 to 90 degrees as well as azimuths between 90 to 270 degrees. The intensity as well as the corresponding tilt and azimuth are then drawn using the “Layout” method in the “graph_objects” module. The colormap used in this plotting was “Portland”, where the warmer color in the contour corresponds the higher solar intensity, which is the ideal angle the solar panel should face to acquire the higher efficiency. The contour was shown in the Figure 6 below. The idea of showing the solar panel installation angles instruction was acquired by the code from Alan Mitchell (Mitchell, 2017)
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis6.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis6.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 6: Solar panel light intensity in different tilts and azimuths</center></p>
                 <p>
                 To better illustrate the relationship between installation tilt and the expected energy produced, the contour was represented into 3d axis using “plotly.graph_objects” module. By converting to the 3d visualization, the impact of tilts and azimuths can be better shown for the user to get the best installation angles. The updated 3d plotting was shown in the Figure 7 below.
                 </p>
-                <img src="https://raw.githubusercontent.com/yuehaoshi/myweb/gh-pages/Pics/SolarVis%20Project/SolarVis7.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
+                <img src="https://raw.githubusercontent.com/yuehaoshi/my-Files/main/WebPics/Solar%20Visualization%20Project/SolarVis7.png" alt = 'Oops, this disappeared..' width = "600" align = "center"/>
                 <p><center>Figure 7: Solar panel light intensity in different tilts and azimuths in 3D</center></p>
             </section>
             <reference className={ styles.reference }>
